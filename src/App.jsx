0fda68ae8aeda912f73dee1c9ac964ee0cf53b70
tamimhasan13 <tamimhasan975@gmail.com>
2026-05-17 16:35:15 +0600

    top Product added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+import { useEffect } from 'react';
 import './App.css'
 import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar/Navbar'
 import Products from './Components/Products/Products'
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TopProducts from './Components/TopPfoducts.jsx/TopProducts';
 function App() {
- 
+ useEffect(() => {
+   AOS.init({
+     offset: 100,
+     duration: 800,
+     easing: "ease-in-sine",
+     delay: 100,
+   });
+   AOS.refresh();
+ }, []);
 
   return (
     <>
      <Navbar></Navbar>
      <Hero></Hero>
      <Products></Products>
+     <TopProducts></TopProducts>
     </>
   )
 }
