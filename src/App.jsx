4675694cae32768a53f718ec520f48cfb05e62c7
tamimhasan13 <tamimhasan975@gmail.com>
2026-05-17 17:53:27 +0600

    footer adde and popup adden

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar/Navbar'
@@ -8,7 +8,15 @@ import "aos/dist/aos.css";
 import TopProducts from './Components/TopPfoducts.jsx/TopProducts';
 import Banner from './Components/Banner/Banner';
 import Subscribe from './Components/Subscribe/Subscribe';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Footer from './Components/Footer/Footer';
+import Popup from './Components/Popup/Popup';
 function App() {
+  const [orderPopup, setOrderPopup] = useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
  useEffect(() => {
    AOS.init({
      offset: 100,
@@ -20,16 +28,19 @@ function App() {
  }, []);
 
   return (
-    <>
-     <Navbar></Navbar>
-     <Hero></Hero>
-     <Products></Products>
-     <TopProducts></TopProducts>
-     <Banner></Banner>
-     <Subscribe></Subscribe>
-     <Products></Products>
-    </>
-  )
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup={handleOrderPopup}></Navbar>
+      <Hero></Hero>
+      <Products></Products>
+      <TopProducts></TopProducts>
+      <Banner></Banner>
+      <Subscribe></Subscribe>
+      <Products></Products>
+      <Testimonials></Testimonials>
+      <Footer></Footer>
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup}></Popup>
+    </div>
+  );
 }
 
 export default App
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from "../../assets/logo.png";
 import DarkMode from "./DarkMode";
 import LowerNavbar from "./LowerNavbar";
 
-const Navbar = () => {
+const Navbar = ({ handleOrderPopup }) => {
   return (
     <div className="mx-auto shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
       <div className="bg-primary/40 py-2">
@@ -27,7 +27,7 @@ const Navbar = () => {
             </div>
             {/* order button */}
             <button
-              onClick={() => alert("Ordering not available yet!")}
+              onClick={()=>handleOrderPopup()}
               className="bg-linear-to-r from-primary to-secondary transition-all duration-200 text-white  py-1 px-4 rounded-full flex items-center gap-3 group"
             >
               <span className="group-hover:block hidden transition-all duration-200">
